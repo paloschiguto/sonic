6024e1a47f26d172f053eed6e75b01cec2b11556
Augusto Paloschi <augusto.paloschi06@gmail.com>
2022-06-22 01:57:37 -0300

    V 0.1 - Iniciando

---
+++ game.py
@@ -0,0 +1,8 @@
+import pygame, random
+pygame.init()
+largura = 960
+altura = 672
+tamanho = (largura, altura)
+pygameDisplay = pygame.display
+pygameDisplay.set_caption("Sonic")
+gameDisplay = pygame.display.set_mode(tamanho)
