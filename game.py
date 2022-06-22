a6994b11edee2a2fccb9073ae9386472b0618aef
Augusto Paloschi <augusto.paloschi06@gmail.com>
2022-06-22 02:00:32 -0300

    V 0.2 - iniciando 02

--- game.py
+++ game.py
@@ -6,3 +6,13 @@ tamanho = (largura, altura)
 pygameDisplay = pygame.display
 pygameDisplay.set_caption("Sonic")
 gameDisplay = pygame.display.set_mode(tamanho)
+gameIcon = pygame.image.load("imagens/ícone.ico")
+pygameDisplay.set_icon(gameIcon)
+bg = pygame.image.load("imagens/background.png")
+bg_destroy = pygame.image.load("imagens/backgroundD.png")
+somMorte = pygame.mixer.Sound("sounds/purr.wav")
+somMorte.set_volume(0.5)
+black = (0, 0, 0)
+white = (255, 255, 255)
+clock = pygame.time.Clock()
+gameEvents = pygame.event
